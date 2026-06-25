e735b2935704c1685563dee63afeadb132315544
Noname <emperador1833@gmail.com>
2026-06-25 00:48:49 -0300

    Update template paths to use APP_ROOT

--- public/list.php
+++ public/list.php
@@ -5,11 +5,11 @@ require_once '../includes/config.php';
 // Verificar si existen registros
 $users = Db::query('SELECT id_user, apellido, nombre, email, telefono FROM user WHERE eliminado = 0;');
 
-$template = DOCUMENT_ROOT . '/templates/user/list.html';
+$template = APP_ROOT . '/templates/user/list.html';
 
 $flashes = null;
 if (Flash::hasFlashes()) {
     $flashes = Flash::getFlashes();
 }
 
-require_once DOCUMENT_ROOT . '/templates/index.html';
+require_once APP_ROOT . '/templates/index.html';
